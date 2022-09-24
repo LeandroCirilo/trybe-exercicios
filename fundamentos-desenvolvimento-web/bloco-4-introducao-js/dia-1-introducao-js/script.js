d9064146ddcf0c64fa31b0bd309e48cfbea19992
Leandro Cirilo <laendor.ricilo@gmail.com>
2022-09-24 19:34:25 -0300

    3/15

--- fundamentos-desenvolvimento-web/bloco-4-introducao-js/dia-1-introducao-js/script.js
+++ fundamentos-desenvolvimento-web/bloco-4-introducao-js/dia-1-introducao-js/script.js
@@ -1,1 +1,69 @@
 console.log('Olá, word!');
+
+//============= Exercicio 01/15 ==============
+
+
+// === Módulo ===
+
+console.log('resultado da Divisão: ', a % b);
+
+// === Divisão ===
+
+console.log('resultado da Divisão: ', a / b);
+
+// === Multiplicação ===
+
+console.log('resultado da Multiplicação: ', a * b);
+
+
+// === Subtração ===
+
+console.log('resultado da Subtração: ', a - b);
+
+
+// === Adicação ====
+
+console.log('resultado da Adição: ', a + b);
+
+//============= Exercicio 02/15 ==============
+
+
+if (a < b) {
+
+    console.log('Maior valor é : ', b);
+
+}
+else if (a > b) {
+
+    console.log('Maior valor é : ', a);
+
+}
+else {
+
+    console.log('Os números tem o mesmo valor');
+
+}
+
+//============= Exercicio 03/15 ==============
+
+const a = 550;
+const b = 1110;
+const c = 100
+
+if (a > b && a > c) {
+
+    console.log('Maior valor é : ', a);
+
+}
+else if (b > a && b > c) {
+
+    console.log('Maior valor é : ', b);
+
+}
+else {
+    console.log('Maior valor é : ', c);
+
+}
+
+//============= Exercicio 04/15 ==============
+
